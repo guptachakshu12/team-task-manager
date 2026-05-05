75c6ce4f136c34f37b1ad0dcfeb8c6235d037ae5
guptachakshu12 <guptachakshu397@gmail.com>
2026-05-05 11:30:37 +0530

    fix railway frontend cors

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,3 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-from database import Base, engine
-from routes.auth_routes import router as auth_router
-from routes.task_routes import router as task_router
-from routes.project_routes import router as project_router
-
-app = FastAPI()
-
-Base.metadata.create_all(bind=engine)
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
@@ -21,16 +9,10 @@ app.add_middleware(
         "http://127.0.0.1:5174",
         "http://127.0.0.1:5175",
         "http://127.0.0.1:5176",
+
+        "https://dazzling-caring-production-0401.up.railway.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-@app.get("/")
-def home():
-    return {"message": "Backend is running 🚀"}
-
-app.include_router(auth_router, prefix="/auth", tags=["Auth"])
-app.include_router(project_router, prefix="/projects", tags=["Projects"])
-app.include_router(task_router, prefix="/tasks", tags=["Tasks"])
