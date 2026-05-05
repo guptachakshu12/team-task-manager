eb22bd4c6b3bf23bfc5f7d999ae07e76ba06d603
guptachakshu12 <guptachakshu397@gmail.com>
2026-05-05 10:31:04 +0530

    connect frontend to backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+const BASE_URL = "https://team-task-manager-production-507c.up.railway.app";
 
 const getToken = () => localStorage.getItem("token");
 
