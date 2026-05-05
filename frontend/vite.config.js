862a7f2e2e80219fc51909efbd771287ad39a875
guptachakshu12 <guptachakshu397@gmail.com>
2026-05-05 11:17:17 +0530

    fix vite host issue for railway

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      "dazzling-caring-production-0401.up.railway.app"
+    ]
+  },
+  preview: {
+    host: true,
+    port: 8080,
+    allowedHosts: [
+      "dazzling-caring-production-0401.up.railway.app"
+    ]
+  }
 })
